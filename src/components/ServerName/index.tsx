ba6cc812dbd0ae9e80e701189c74565bba0928a2
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-07-23 09:56:35 -0300

    Server Name

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Title, ExpandIcon } from './style'
+
+const ServerName: React.FC = () => {
+    return(
+        <Container>
+            <Title>
+                 Servidor do Pedro 
+                 <ExpandIcon />
+            </Title>
+        </Container>
+    )
+}
+
+export default ServerName;
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Grid } from './style'
 import ServerList from '../ServerList'
+import ServerName from '../ServerName'
 
 const Layout: React.FC = () => {
     return(
          <Grid>
             <ServerList />
+            <ServerName />
          </Grid>
 
     )
