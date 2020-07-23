8bd33ab4d8ce74898c87ae68f9de3f78d9a68922
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-07-22 21:44:02 -0300

    Estilos Globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import GlobalStyleComponent from './globalStyles'
 
 function App() {
   return(
-    <h1>Hello World</h1>
+    <>
+      <h1> Hello World </h1>
+
+      <GlobalStyleComponent />
+
+    </>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import GlobalStyleComponent from './globalStyles'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
 
-    <GlobalStyleComponent />
   </React.StrictMode>,
   document.getElementById('root')
 )
